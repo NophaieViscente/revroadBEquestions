80f03e34a273104943504ebf085d667e20a325a1
Nophaie Viscente Morais <nophaie.morais@gria.io>
2024-02-02 15:43:34 -0300

    BUGFIX : Adjusting name of flask app

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ URI = config("URI")
 PASSWORD = "passw0rd"
 USER = "neo4j"
 
-neo4j = Flask(__name__)
+app = Flask(__name__)
 graphBuilder = GraphHandler(uri=URI, user=USER, password=PASSWORD)
-CORS(app=neo4j)
+CORS(app=app)
 
 
-@neo4j.route("/insert-question", methods=["POST"])
+@app.route("/insert-question", methods=["POST"])
 def add_question():
     request_data = request.get_json()
     question_id = request_data.get("id")
@@ -22,19 +22,19 @@ def add_question():
     return json.dumps({"message": response})
 
 
-@neo4j.route("/delete-question/<id>", methods=["DELETE"])
+@app.route("/delete-question/<id>", methods=["DELETE"])
 def delete_question(id: str):
     response = graphBuilder.delete_node(id=id)
     return json.dumps({"message": response})
 
 
-@neo4j.route("/find-next-question/<id>/<choice>", methods=["GET"])
+@app.route("/find-next-question/<id>/<choice>", methods=["GET"])
 def find_next_question(id: str, choice):
     response = graphBuilder.search_next_node(id=id, choice=choice)
     return json.dumps({"next_node": response})
 
 
-@neo4j.route("/create-edge", methods=["POST"])
+@app.route("/create-edge", methods=["POST"])
 def create_edge_question():
     request_data = request.get_json()
     source_id = request_data.get("source")
@@ -47,7 +47,7 @@ def create_edge_question():
     return json.dumps({"message": response})
 
 
-@neo4j.route("/update-edge", methods=["POST"])
+@app.route("/update-edge", methods=["POST"])
 def update_edge_question():
     request_data = request.get_json()
     choice = request_data.get("choice")
@@ -60,11 +60,11 @@ def update_edge_question():
     return f"Updated Edge {edgeId}"
 
 
-@neo4j.route("/delete-edge/<edgeId>", methods=["DELETE"])
+@app.route("/delete-edge/<edgeId>", methods=["DELETE"])
 def delete_node_edge(edgeId: str):
     response = graphBuilder.delete_edge_node(edgeId=edgeId)
     return json.dumps({"message": response})
 
 
-if __name__ == "__main__":
-    neo4j.run(debug=True)
+# if __name__ == "__main__":
+# app.run(debug=True)
